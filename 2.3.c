dbace7b901e52b91ba5a870be64a97d627753776
pedroldev <ploraschi29@gmail.com>
2026-04-08 14:41:40 -0300

    update

---
+++ 2.3.c
@@ -0,0 +1,9 @@
+/*2.3 Cargar de forma aleatoria una matriz de 5 x 8 con números de 2 cifras. Determinar el máximo valor
+almacenado en la matriz indicando cuantas veces se repite y en que posiciones.*/
+
+#include <stdio.h> 
+
+int main(){
+
+    return 0;
+}
