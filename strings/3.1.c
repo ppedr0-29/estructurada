8e1b8bb87bebe158a8f8aea25a732ee6ed27080f
pedroldev <ploraschi29@gmail.com>
2026-04-16 18:58:05 -0300

    update

---
+++ strings/3.1.c
@@ -0,0 +1,3 @@
+/*3.1 Ingresar una frase de hasta 500 caracteres y contar cuántas palabras contiene dicha frase.
+a. Considerar que las palabras están separadas por un único espacio.
+b. Considerar que las palabras pueden estar separadas por más de un espacio consecutivo.*/
