53515d934da8a160dd7cfe0946c492125d3b8458
kamichate <kamipacha2@gmail.com>
2026-04-27 17:17:27 -0300

    update

---
+++ structs/4.5.c
@@ -0,0 +1,169 @@
+/*4.5 De una persona se conoce los siguientes datos:
+• Número de Legajo (int, entre 1 y 10.000)
+• Apellido (string, 15 caracteres)
+• Nombre (string, 15 caracteres)
+• Altura (float, mayor de 0)
+• Sexo (char, F o M)
+• Día de Nacimiento (int, entre 1 y 31
+• Mes de Nacimiento (int, ente 1 y 12)
+• Año de Nacimiento (int, ente 1950 y 2019)
+Se solicita realizar los siguientes procesos:
+a. Con datos ingresados por teclado: Crear una Estructura de Datos con los datos enunciados
+anteriormente y llamarla PERSONA.
+b. Mostrar los datos de la Estructura de Datos del punto a.
+c. Con datos ingresados por teclado: Modificar la Estructura de Datos del punto a., crear una Estructura
+de Datos que contenga Nombre y Apellido y otra Estructura de Datos que contenga Día/Mes/Año
+utilizarla en una nueva Estructurad de Datos llamada PERSONA2.
+d. Mostrar los datos de la Estructura de Datos del punto c.
+e. Crear una FUNCION que cargue los datos de la Estructurad de Datos del punto c y que retorne los datos
+al programa principal (main).
+f. Con datos ingresados por teclado: Modificar la Estructurad de Datos llamada PERSONA2 del punto c.
+y agregarle un campo/miembro que contenga los códigos de las Materias Aprobadas (pueden llegar a
+ser hasta 36 materias) y llamarla PERSONA3.
+g. Crear una FUNCION que realice el punto f.
+h. Mostrar los datos de la Lista creada en el punto f.
+i. Crear una FUNCION que realice el punto h.*/
+
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+
+
+typedef struct
+{
+    int numLegajo;  // entre 1 y 10000
+    char apell [16]; // de 15
+    char nombre [16];  // de 15
+    float altura; // mayor a 0
+    char sex; // F, o M
+    int dia; // entre 1 y 312¿
+    int  mes; // entre 1 y 12
+    int anio; // entre 1950 y 2019
+}PERSONA;
+
+
+int main()
+{
+    PERSONA datos;
+    return 0;
+}
+
+void IngresarDatos (PERSONA datos){
+
+    int d,m,a;
+    printf("Ingrese el numero de Legajo:");
+    datos.numLegajo=leeyValidaIntEntre2(1, 10000);
+    printf("\n Ingrese el apellido:");
+    fflush(stdin);
+    leerTexto(datos.apell, 15);
+    validarVacio(datos.apell, 15);    
+    printf("\n Ingrese el nombre:");
+    fflush(stdin);
+    leerTexto(datos.nombre, 15);
+    validarVacio(datos.nombre, 15);    
+    printf("Ingrese la altura:");
+    datos.altura=leeyValidaFloat(0);
+    printf("Ingrese el sexo:");
+    datos.sex=validarSexo(datos);
+    printf("Ingrese la fecha con el siguiente formato (dd/mm/aaaa):");
+    scanf("%d/%d/%d",&d, &m, &a);
+    while(validarFecha(d,m,a)==false){
+        printf("fecha invalida, reingrese:");
+        scanf("%d/%d/%d",&d, &m, &a);
+    }
+    datos.dia=d;
+    datos.mes=m;
+    datos.anio=a;
+    
+    
+
+
+}
+
+int leeyValidaIntEntre2(int min, int max){
+    int dato;
+    scanf("%d", &dato);
+    getchar();
+    while (dato<min || dato>max){
+        printf("\nERROR - El dato ingresado tiene que encontrarse dentro del rango[%d-%d]: \t", min, max);
+        scanf("%d", &dato);
+        getchar();
+    }
+    return dato;
+}
+
+float leeyValidaFloat(float lim){
+    float dato;
+    scanf("%f", &dato);
+    getchar();
+    while (dato<lim){
+        printf("\nERROR - La altura ingresada tiene que ser mayor a %f: \t", lim);
+        scanf("%f", &dato);
+        getchar();
+    }
+    return dato;
+}
+
+void leerTexto(char texto[], int largo) {
+    int i;
+    fgets(texto, largo, stdin); 
+    i = 0;
+    while (texto[i] != '\0') {
+        if (texto[i] == '\n') { 
+            texto[i] = '\0';
+        } else {
+            i++;
+        }
+    }
+}
+
+void validarVacio(char texto[], int largo) {
+    while (strlen(texto) == 0) {
+        printf("El texto no puede estar vacio. Ingrese nuevamente: ");
+        leerTexto(texto, largo);
+    }
+}
+
+char validarSexo(PERSONA datos){
+    char dato;
+    scanf("%c", &dato);
+    dato=toupper(dato);
+    while (dato!='F'|| dato!='M')
+    {
+        printf("\n El dato no es F o M. Ingrese nuevamente:");
+        scanf("%c", &dato);
+        dato=toupper(dato);
+    }
+    return dato;
+}
+
+bool validarFecha(int dia, int mes, int anio){
+    bool fechaValida = false;
+    if (anio> 1900){
+        if (mes >=1 && mes <=12){
+            if (dia>=1 && dia <= cantDiasMes(mes, anio)){
+                fechaValida = true;
+            }
+        }
+    } return fechaValida;
+}
+int cantDiasMes(int mes, int anio){
+    int cantDias =31;
+    if(mes == 4 || mes == 6 || mes == 9 || mes == 11){
+        cantDias = 30;
+    }else{
+        if(mes==2){
+            cantDias= 28 + esBisiesto(anio);
+        }
+    }
+        
+    return cantDias;
+}
+int esBisiesto(int anio){
+    int bisiesto =0;
+    if ((anio%4==0 && anio%100!=0)||(anio%400==0))
+        bisiesto = 1;
+    return bisiesto;
+}
