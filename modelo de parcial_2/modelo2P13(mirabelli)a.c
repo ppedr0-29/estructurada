548cd5e7925968c37a7a4f066f64690593e1a3c8
pedroldev <ploraschi29@gmail.com>
2026-06-21 19:48:35 -0300

    update

--- modelo de parcial_2/modelo2P13(mirabelli)a.c
+++ modelo de parcial_2/modelo2P13(mirabelli)a.c
@@ -1,23 +1,18 @@
-/*Una empresa necesita liquidar los sueldos de sus empleados y para ello dispone del archivo personal.dat con la siguiente estructura:
-
-Código de Empleado (string de 3 caracteres)
-
-Nombre Empleado (string de 30 caracteres)
-
-Categoría (A – Aprendiz, D – Directivo, E – Especialista, T – Técnico)
-
-Importe por hora que cobra (real entre 1000 y 5000)
-
-Sueldo a cobrar (real)
-
-Luego por cada empleado se ingresarán por teclado el Código de Empleado y las horas que trabajó en el mes (entero de 0 a 168). En base a las horas trabajadas y el importe que cobra por hora se debe actualizar el sueldo a cobrar en el archivo. En caso de no encontrar el Empleado en el archivo debe guardarse el error en un archivo llamado Errores.dat con la siguiente estructura (Código de Empleado y Horas Trabajadas).
-
-La liquidación de los sueldos termina con el ingreso de un empleado igual a "FIN".
-Además, se necesita determinar e informar:
-
-El nombre del empleado que cobra el máximo sueldo actualizado, en caso de ser más de uno mostrar el último.
-
-El importe total a pagar por cada categoría (resolver en una función).
-
-Validar todos los datos que ingresan por teclado según lo indicado.
-El código debe estar correctamente identado.*/
+/*La empresa TRASH dispone de un archivo llamado Stock.dat que contiene la información de los productos 
+que comercializa.
+El archivo contiene la siguiente información:
+Código de artículo (string de 3 caracteres)
+Descripción (50 caracteres máximo)
+Stock (entero)
+Luego se ingresan por teclado las compras realizadas indicando:
+Código de artículo (string de 3 caracteres)
+Cantidad (entero mayor a cero)
+La carga por teclado de las compras finaliza con un código de artículo "FIN".
+Por cada compra se debe controlar si existe el articulo (resolver en una Función llamada BUSCAR) y si existe, 
+actualizar el stock de dicho producto, sino existe el articulo grabar un registro en un archivo Errores.dat 
+con la cantidad que no pudo registrar, dicho registro debe contener:
+Código de artículo
+Cantidad
+Si ya hay un registro previo en dicho archivo de errores con el mismo artículo debe incrementarse la cantidad. 
+Informar al final la cantidad de artículos actualizados y la cantidad de errores utilizando la función INFORME.
+Validar todos los datos que ingresan por teclado según lo indicado.*/
---
+++ modelo de parcial_2/modelo2P14(mirabelli)a.c
@@ -0,0 +1,21 @@
+/*/*Una empresa necesita liquidar los sueldos de sus empleados y para ello dispone del archivo personal.dat con la siguiente estructura:
+
+Código de Empleado (string de 3 caracteres)
+
+Nombre Empleado (string de 30 caracteres)
+
+Categoría (A – Aprendiz, D – Directivo, E – Especialista, T – Técnico)
+
+Importe por hora que cobra (real entre 1000 y 5000)
+
+Sueldo a cobrar (real)
+
+Luego por cada empleado se ingresarán por teclado el Código de Empleado y las horas que trabajó en el mes (entero de 0 a 168). 
+En base a las horas trabajadas y el importe que cobra por hora se debe actualizar el sueldo a cobrar en el archivo. En caso de no encontrar el Empleado en el archivo debe guardarse el error en un archivo llamado Errores.dat con la siguiente estructura (Código de Empleado y Horas Trabajadas).
+
+La liquidación de los sueldos termina con el ingreso de un empleado igual a "FIN".
+Además, se necesita determinar e informar:
+El nombre del empleado que cobra el máximo sueldo actualizado, en caso de ser más de uno mostrar el último.
+El importe total a pagar por cada categoría (resolver en una función).
+Validar todos los datos que ingresan por teclado según lo indicado.
+El código debe estar correctamente identado.*/
