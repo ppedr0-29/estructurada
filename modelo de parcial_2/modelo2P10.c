3b51dfb8877683ffcd63b55559938a8f00dddb0d
pedroldev <ploraschi29@gmail.com>
2026-06-22 11:05:38 -0300

    update

--- modelo de parcial_2/modelo2P10.c
+++ modelo de parcial_2/modelo2P10.c
@@ -1,7 +1,13 @@
-/*El DIIT quiere actualizar la información de los alumnos que han cursado este cuatrimestre. Para ello dispone de 2 archivos. El primer archivo se llama alumnos.dat y tiene registros con la siguiente estructura: DNI (entero), nombre y apellido (texto, 50 caracteres máximo), cantidad total de materias cursadas desde que ingresó a la facultad (entero), año de última cursada (entero), cuatrimestre de la última cursada (entero).
+/*El DIIT quiere actualizar la información de los alumnos que han cursado este cuatrimestre. Para ello dispone de 2 archivos. 
+El primer archivo se llama alumnos.dat y tiene registros con la siguiente estructura: DNI (entero), nombre y apellido (texto, 50 caracteres máximo),
+cantidad total de materias cursadas desde que ingresó a la facultad (entero), año de última cursada (entero), cuatrimestre de la última cursada (entero).
 
-Por otro lado, se tiene el archivo CursadaPrimCuat.dat con la información de los alumnos que han cursado el primer cuatrimestre del 2025, este archivo contiene dos campos, DNI (entero) y código de materia (entero), de los alumnos que se han inscripto a las diferentes materias. El alumno puede inscribirse en ninguna o varias materias. Este archivo no se encuentra ordenado. Con estos dos archivos debe:
+Por otro lado, se tiene el archivo CursadaPrimCuat.dat con la información de los alumnos que han cursado el primer cuatrimestre del 2025, 
+este archivo contiene dos campos, DNI (entero) y código de materia (entero), de los alumnos que se han inscripto a las diferentes materias. 
+El alumno puede inscribirse en ninguna o varias materias. Este archivo no se encuentra ordenado. Con estos dos archivos debe:
 
-Actualizar el archivo alumnos.dat con los alumnos que han cursado el primer cuatrimestre del 2025. Si se trata de un alumno nuevo (ingresante), se deben solicitar los datos faltantes para poder agregar al archivo de alumnos.
+Actualizar el archivo alumnos.dat con los alumnos que han cursado el primer cuatrimestre del 2025. 
+Si se trata de un alumno nuevo (ingresante), se deben solicitar los datos faltantes para poder agregar al archivo de alumnos.
 
-Realizar un informe de aquellos alumnos que no han cursado el primer cuatrimestre del 2025, indicando DNI, nombre y apellido, cuatrimestre y año de última cursada.*/
+Realizar un informe de aquellos alumnos que no han cursado el primer cuatrimestre del 2025, indicando DNI, nombre y apellido, cuatrimestre y 
+año de última cursada.*/
--- modelo de parcial_2/modelo2P7.c
+++ modelo de parcial_2/modelo2P7.c
@@ -11,4 +11,112 @@ Mayor a 10 años 5%
 Mayor a 20 años 6%
 Una vez actualizado se debe generar un nuevo archivo, que solo contenga a los empleados del área administrativa
 e informar la cantidad de registros de dicho archivo, utilizando las funciones vistas en el curso.
+
+abrir archivo r+b
+fread()
+while(!feof){
+    if(antiguedad>10 && antiguedad<=20)
+    datos.sueldo*=1.05
+    else if(antiguedad>20){
+        datos.sueldo*=1.06
+    }
+
+}
+
+nuevoArchivo(){
+    FILE *arch en rb;
+    FILE *archN en wb;
+    EMP dato;
+    fread
+    while(!feof){
+        if(datos.area=='A'){
+            fwrite(&dato, archN);
+            cantRegistro++;
+        }
+        fread();    
+    }
+    fclose(arch)
+    fclose(archN)
+}
+
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+typedef struct
+{
+    int legajo;
+    char nombre[46];
+    int antig;
+    float sueldo;
+    char area;
+}EMP;
+
+FILE *abrirArchivo(char [], char []);
+void procesarArchivo();
+void nuevoArchivo();
+
+int main(){
+    procesarArchivo();
+    nuevoArchivo();
+
+    return 0;
+}
+
+void nuevoArchivo(){
+    FILE*arch=abrirArchivo("sueldos.dat", "rb");
+    FILE *archNuev=abrirArchivo("empleadosA.dat", "wb");
+    EMP datos;
+    int cantRegistros=0;
+    fread(&datos, sizeof(datos), 1, arch);
+    while (!feof(arch))
+    {
+        if (datos.area=='A')
+        {
+            fwrite(&datos, sizeof(datos), 1, archNuev);
+            cantRegistros++;
+        }
+        fread(&datos, sizeof(datos), 1, arch);
+    }
+    printf("La cantidad de registros son %d del archivo empleadosA.dat", cantRegistros);
+
+    fclose(arch);
+    fclose(archNuev);
+}
+
+void procesarArchivo(){
+    FILE *arch;
+    arch=abrirArchivo("sueldos.dat", "r+b");
+    EMP datos;
+    fread(&datos, sizeof(datos), 1, arch);
+    while (!feof(arch))
+    {
+        if (datos.antig>10 && datos.antig<=20)
+        {
+            datos.sueldo=(datos.sueldo*1.05);
+        }else if(datos.antig>20){
+            datos.sueldo=(datos.sueldo*1.06);
+        }
+        fseek(arch, -sizeof(datos), SEEK_CUR);
+        fwrite(&datos, sizeof(datos), 1, arch);
+        fflush(arch);
+        fread(&datos, sizeof(datos), 1, arch);
+    }
+    
+    fclose(arch);
+}
+
+FILE *abrirArchivo(char nombre[], char tipo[]){
+    FILE *arch;
+    arch=fopen(nombre, tipo);
+    if (arch==NULL)
+    {
+        printf("Error.");
+        getch();
+        exit(1);
+    }
+    return arch;
+}
